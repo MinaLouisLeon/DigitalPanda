e479df45e6eb601d4c8be823519784b00461dbf5
Mina Louis <minalouisleon@gmail.com>
2024-02-18 18:36:50 +0200

    add cart components

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -49,7 +49,7 @@ const NavItem = ({category,handleOpen,isOpen,isAnyOpen}: NavItemProps) => {
                       <Link href={item.href} className="mt-6 block font-medium text-gray-900">
                         {item.name}
                       </Link>
-                      <p className="mt-1" aria-hidden={true} >Shop now</p>
+                      <p className="mt-1" aria-hidden="true">Shop now</p>
                     </div>
                   ))}
                 </div>
